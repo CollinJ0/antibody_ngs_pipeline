5cb285f9be233a0addaf08ebb3d51a4eafb41e14
Collin Joyce <cjoyce@scripps.edu>
2018-09-25 13:53:53 -0700

    fixed-bug

--- antibody_ngs_pipeline/horseshoe.py
+++ antibody_ngs_pipeline/horseshoe.py
@@ -199,9 +199,9 @@ def run_abstar(parameters, project):
             print('ERROR: No Files Found in Basemount!')
             sys.exit(4)
     if parameters.merge == False:
-    print("\n========================================" \
-          "\nUnzipping Input Files\n" \
-          "========================================\n")
+        print("\n========================================" \
+              "\nUnzipping Input Files\n" \
+              "========================================\n")
         os.system('gunzip {}/input/*'.format(parameters.project_dir))
     run_standalone(parameters)
 
